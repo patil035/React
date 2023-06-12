ab85b763d50fdac0915a78d9beb959b340fbcaba
Ravindra <patil.ravi035@gmail.com>
2023-06-12 11:40:29 +0530

    React Foundation

--- npm-react/App.js
+++ npm-react/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = React.createElement(
-  "h1",
-  {
-    style: {
-      fontWeight: "bold",
-    },
-  },
-  "React with NPM packages"
+const title = <h2 id="title">Title of the component</h2>;
+
+const Heading = () => (
+  <div>
+    {title}
+    <h1>Heading of the component</h1>
+  </div>
+);
+
+const Footer = () => <div>Footer of the component</div>;
+
+const InitialComponent = () => (
+  <React.Fragment>
+    <Heading></Heading>
+    <div>Content of the component</div>
+    <Footer></Footer>
+  </React.Fragment>
 );
 
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
 
-reactRoot.render(element);
+reactRoot.render(<InitialComponent></InitialComponent>);
